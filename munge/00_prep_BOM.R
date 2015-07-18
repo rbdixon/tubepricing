c307bcabe14ade986a2085c144c63d73de9601b3
Brad Dixon <rbdixon@gmail.com>
2015-07-18 13:33:45 -0400

    Add PARTS table built from BOM.

--- munge/00_prep_BOM.R
+++ munge/00_prep_BOM.R
@@ -6,3 +6,7 @@ BOM = ldply( seq(2,16,2), function(ci) {
   tbl_df %>% 
   na.omit() %>% 
   mutate_each( funs(factor), tube_assembly_id, component)
+
+PARTS = BOM %>% 
+  group_by( tube_assembly_id ) %>% 
+  summarize( num_parts = sum(quantity) )
